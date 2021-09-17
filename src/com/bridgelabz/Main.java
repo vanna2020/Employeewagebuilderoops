25372d21711dcfab73a8eb993a86780ba588f570
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 22:50:30 -0700

    using switch case

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,26 +1,30 @@
 package com.bridgelabz;
 import java.util.Random;
  public class Main {
+     public static final int IS_FULLTIME = 1;
+     public static final int IS_PARTTIME = 2;
+     public static final int Emp_Rate_PerHr = 20;
+
      public static void main(String[] args) {
          System.out.println("Welcome To Employee Wage Computation Program");
-
-         int IS_FULLTIME = 1;
-         int IS_PARTTIME = 2;
-         int Emp_Rate_PerHr = 20;
          // Declaring a variables
          int empHrs = 0;
          int empWage = 0;
          /*
-          * Using Math.random method to generate random numbers 0, 1 and 2
+          * Using random method to generate random numbers 0, 1 and 2
           */
          Random random = new Random();
          int empCheck = random.nextInt(3);
-         if (empCheck == IS_FULLTIME)
-             empHrs = 8;
-         else if (empCheck == IS_PARTTIME)
-             empHrs = 4;
-         else
-             empHrs = 0;
+         switch (empCheck) {
+             case IS_FULLTIME: // FullTime Employee
+                 empHrs = 8;
+                 break;
+             case IS_PARTTIME: // PartTime Employee
+                 empHrs = 4;
+                 break;
+             default: // Employee is absent
+                 empHrs = 0;
+         }
          /*
           * Calculating the daily employee wage of an employee
           */
