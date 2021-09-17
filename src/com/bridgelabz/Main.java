6fbaefb53c0995468ffff685cf1b1a60c89ac907
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 22:42:07 -0700

    employee daily wage

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,18 +1,21 @@
 package com.bridgelabz;
 import java.util.Random;
  public class Main {
-                    public static void main(String[] args) {
-                        System.out.println("Welcome To Employee Wage Computation Program");
+     public static void main(String[] args) {
+         System.out.println("Welcome To Employee Wage Computation Program");
+         int IS_FULLTIME = 1;
+         int Emp_Rate_PerHr = 20;
 
-                        int IS_FULLTIME = 1;
+         int empHrs = 0;
+         int empWage = 0;
 
-                        Random random = new Random();
-                        int empCheck = random.nextInt(2);
-                        if (empCheck == IS_FULLTIME)
-                            System.out.println("The Employee is Present");
-                        else
-                            System.out.println("The Employee is Absent");
-
-                    }
-                }
-
+         Random random = new Random();
+         int empCheck = random.nextInt(2);
+         if (empCheck == IS_FULLTIME)
+             empHrs = 8;
+         else
+             empHrs = 0;
+         empWage = Emp_Rate_PerHr * empHrs;
+         System.out.println("The daily wage of an employee is : " + empWage);
+     }
+ }
