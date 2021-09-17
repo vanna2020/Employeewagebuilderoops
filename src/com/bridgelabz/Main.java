2ed2146bd127914e4078101e20fb7bdd8ad2792c
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 22:32:46 -0700

    Check employee attandance

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,8 +1,18 @@
 package com.bridgelabz;
-
+import java.util.Random;
  public class Main {
+                    public static void main(String[] args) {
+                        System.out.println("Welcome To Employee Wage Computation Program");
 
-            public static void main(String[] args) {
-                System.out.println("Welcome To Employee Wage Computation Program");
-            }
-        }
+                        int IS_FULLTIME = 1;
+
+                        Random random = new Random();
+                        int empCheck = random.nextInt(2);
+                        if (empCheck == IS_FULLTIME)
+                            System.out.println("The Employee is Present");
+                        else
+                            System.out.println("The Employee is Absent");
+
+                    }
+                }
+
