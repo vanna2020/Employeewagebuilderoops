4d2c5f08590c0c807f0a192302734a939af85208
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 23:06:54 -0700

    using methods and classes

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -1,16 +1,14 @@
 package com.bridgelabz;
 import java.util.Random;
  public class Main {
-
      public static final int IS_FULLTIME = 1;
      public static final int IS_PARTTIME = 2;
      public static final int Emp_Rate_PerHr = 20;
      public static final int NO_WORKINGDAYS = 20;
      public static final int MAX_HRS_MONTH = 100;
 
-     public static void main(String[] args) {
-         System.out.println("Welcome To Employee Wage Computation Program");
-         // Declaring a variables
+     public static int computeWage() {
+         // Declaring the variables
          int empHrs = 0;
          int empWage = 0;
          int totalEmpWage = 0;
@@ -30,7 +28,7 @@ import java.util.Random;
                  case IS_PARTTIME: // PartTime Employee
                      empHrs = 4;
                      break;
-                 default: // Employee is absent
+                 default:  // Employee is absent
                      empHrs = 0;
              }
              empWage = Emp_Rate_PerHr * empHrs;
@@ -39,6 +37,12 @@ import java.util.Random;
                      ", Employee Wage : " + empWage);
          }
          totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
-         System.out.println("Total Wages of an Employee is : " + totalEmpWage);
+         System.out.println("Total Wages is : " + totalEmpWage);
+         return totalEmpWage;
+     }
+
+     public static void main(String[] args) {
+         System.out.println("Welcome To Employee Wage Computation Program");
+         computeWage();
      }
  }
