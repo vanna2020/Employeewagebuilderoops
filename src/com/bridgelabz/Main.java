bd96891d5b88f3a467084e20dbfe9ded34e6d91d
vanna2020 <vannasingh1999@gmail.com>
2021-09-16 22:47:17 -0700

    part time employee

--- src/com/bridgelabz/Main.java
+++ src/com/bridgelabz/Main.java
@@ -3,18 +3,27 @@ import java.util.Random;
  public class Main {
      public static void main(String[] args) {
          System.out.println("Welcome To Employee Wage Computation Program");
-         int IS_FULLTIME = 1;
-         int Emp_Rate_PerHr = 20;
 
+         int IS_FULLTIME = 1;
+         int IS_PARTTIME = 2;
+         int Emp_Rate_PerHr = 20;
+         // Declaring a variables
          int empHrs = 0;
          int empWage = 0;
-
+         /*
+          * Using Math.random method to generate random numbers 0, 1 and 2
+          */
          Random random = new Random();
-         int empCheck = random.nextInt(2);
+         int empCheck = random.nextInt(3);
          if (empCheck == IS_FULLTIME)
              empHrs = 8;
+         else if (empCheck == IS_PARTTIME)
+             empHrs = 4;
          else
              empHrs = 0;
+         /*
+          * Calculating the daily employee wage of an employee
+          */
          empWage = Emp_Rate_PerHr * empHrs;
          System.out.println("The daily wage of an employee is : " + empWage);
      }
